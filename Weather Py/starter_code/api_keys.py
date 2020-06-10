c2f64b3f72eea1c65d0e106b22a05eff10f9144c
asamuelcole <63174579+asamuelcole@users.noreply.github.com>
2020-06-10 00:05:17 -0500

    Add files via upload

---
+++ Weather Py/starter_code/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "c0af52017216b219e16aecb2ae1d8e2b"
+
+# Google API Key
+g_key = "AIzaSyC5QIAlfHyepF7aFbVlkieV1jr8F-BLXcc"
+
+# g_key = "AIzaSyB26ON7_Zf1eDd3X9V8Jd58VBgrY-279h0"
